27535a935f51c4cbd83e7ead562eddf1a056eb87
maxwellarmah01 <maxwellarmah01@gmail.com>
2022-09-13 18:00:42 -0700

    create files 0 to 11

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,53 @@
+/*
+ *
+ *  * File: 0-putchar.c
+ *
+ *   * Auth: maxwellarmah01
+ *
+ *    *
+ *
+ *     */
+
+
+
+#include <stdio.h>
+
+
+
+/**
+ *
+ *  * main - Prints "_putchar" followed by a new line.
+ *
+ *   *
+ *
+ *    * Return: Always 0.
+ *
+ *     */
+
+int main(void)
+
+{
+
+		0-putchar('_');
+
+			0-putchar('p');
+
+				0-putchar('u');
+
+					0-putchar('t');
+
+						0-putchar('c');
+
+							0-putchar('h');
+
+								0-putchar('a');
+
+									0-putchar('r');
+
+										0-putchar('\n');
+
+
+
+											return (0);
+
+}
