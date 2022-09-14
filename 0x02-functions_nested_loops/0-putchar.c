1e3fd60ce36bd2b47876111c27a118beb1a0a310
maxwellarmah01 <72176807+maxwellarmah01@users.noreply.github.com>
2022-09-14 01:58:23 +0000

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,18 +1,4 @@
-/*
- *
- *  * File: 0-putchar.c
- *
- *   * Auth: maxwellarmah01
- *
- *    *
- *
- *     */
-
-
-
-#include <stdio.h>
-
-
+#include "main.h"
 
 /**
  *
@@ -22,32 +8,27 @@
  *
  *    * Return: Always 0.
  *
- *     */
-
+ **/
 int main(void)
-
 {
 
-		0-putchar('_');
+	0-putchar('_');
 
-			0-putchar('p');
+	0-putchar('p');
 
-				0-putchar('u');
+	0-putchar('u');
 
-					0-putchar('t');
+	0-putchar('t');
 
-						0-putchar('c');
+	0-putchar('c');
 
-							0-putchar('h');
+	0-putchar('h');
 
-								0-putchar('a');
+	0-putchar('a');
 
-									0-putchar('r');
+	0-putchar('r');
 
-										0-putchar('\n');
+	0-putchar('\n');
 
-
-
-											return (0);
-
+	return (0);
 }
