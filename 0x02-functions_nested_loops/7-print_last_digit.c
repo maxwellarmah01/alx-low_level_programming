70b65e1f7343ecafe74924b71c88ed4469691a92
maxwellarmah01 <72176807+maxwellarmah01@users.noreply.github.com>
2022-09-14 01:40:03 +0000

    Update 7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -15,13 +15,13 @@ int print_last_digit(int n)
 
 		a = n % 10;
 
-		if (a < 0)
-		{
-			a = -a;
+	}
+	if (a < 0)
+	{
+		a = -a;
 
-			_putchar(a + '0');
+		_putchar(a + '0');
 
-			return (a);
-		}
+		return (a);
 	}
 }
